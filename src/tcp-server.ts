397a77b7b7ff08bc73111dc3cddf7decc9da8f7f
maham <mahaampakzad@gmail.com>
2026-09-22 02:32:12 +0330

    feat: implement basic TCP echo server

--- src/tcp-server.ts
+++ src/tcp-server.ts
@@ -3,10 +3,16 @@ import net from "node:net";
 const PORT = Number(process.env.PORT) || 9000;
 
 const server = net.createServer((socket) => {
-  console.log("Client connected");
+  console.log(
+    ` ${socket.remoteAddress} on port ${socket.remotePort} connected on family ${socket.remoteFamily} to ${socket.localAddress} on port ${socket.localPort}`,
+  );
   socket.on("data", (data) => {
     console.log(`Received data: ${data}`);
     socket.write(`Echo: ${data}`);
+  });
+
+  socket.on("error", (error) => {
+    console.error("Socket error:", error);
   });
 
   socket.on("end", () => {
@@ -14,12 +20,6 @@ const server = net.createServer((socket) => {
   });
 });
 
-server.on("connection", (socket) => {
-  console.log(
-    ` ${socket.remoteAddress} on port ${socket.remotePort} connected on family ${socket.remoteFamily} to ${socket.localAddress} on port ${socket.localPort}`,
-  );
-});
-
 server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT} `);
+  console.log(`Server listening on port ${PORT}`);
 });
